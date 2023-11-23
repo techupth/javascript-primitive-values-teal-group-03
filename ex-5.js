e39a448d73c4b95ffaa0ed1b788ff78238f6c229
tanasri <tanawan95@gmail.com>
2023-11-23 12:58:52 +0700

    completed exercise

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,7 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+
+const productPrice = 27000;
+const discount = (25 / 100) * 27000;
+const priceAfterDiscount = productPrice - discount;
